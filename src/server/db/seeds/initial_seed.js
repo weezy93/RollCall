bcd319195a64986c460afb28bd7fe71bf4d6c690
BenAychh <ben.tomas.hernandez@gmail.com>
2016-03-14 16:24:54 -0600

    Events seeding

--- src/server/db/seeds/initial_seed.js
+++ src/server/db/seeds/initial_seed.js
@@ -1,12 +1,17 @@
+var seeder = require('knex-csv-seeder').seeder.seed;
+exports.seed = seeder({
+  table: 'events',
+  file: './src/server/db/csv/events.csv',
+});
 
-exports.seed = function(knex, Promise) {
-  return Promise.join(
-    // Deletes ALL existing entries
-    knex('table_name').del(), 
-
-    // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
-  );
-};
+// exports.seed = function(knex, Promise) {
+//   return Promise.join(
+//     // Deletes ALL existing entries
+//     knex('table_name').del(),
+//
+//     // Inserts seed entries
+//     knex('table_name').insert({id: 1, colName: 'rowValue'}),
+//     knex('table_name').insert({id: 2, colName: 'rowValue2'}),
+//     knex('table_name').insert({id: 3, colName: 'rowValue3'})
+//   );
+// };
