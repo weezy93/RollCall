66551d9fe500ea8bfe5d9d32801d66c711e76567
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-14 15:17:52 -0600

    Fixed up local auth and added connect-flash

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 var helpers = require('../lib/helpers');
+var passport = require('../lib/auth');
+function Teachers() {
+  return knex('teachers');
+}
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var user = req.user;
+  var messages = req.flash('message');
+  res.render('index', { title: 'Express', user: user, messages: messages });
 });
 
 router.get('/sales', function(req, res, next) {
@@ -28,15 +34,15 @@ router.get('/addEvent', function(req, res, next) {
 //   res.json(req.body);
 // });
 
-router.get('/teacher/add', helpers.loginRedirect, function(req, res, next) {
+router.get('/teacher/add', function(req, res, next) {
   var message = req.flash('message') || '';
   res.render('addTeacher', {title: 'Add Teacher', messages: message});
 });
 
 router.post('/teacher/add', function(req, res, next) {
-  var username = req.body.username;
+  var email = req.body.email;
   var password = req.body.password;
-  Teachers().where('email', req.body.username).then(function(data) {
+  Teachers().where('email_address', req.body.email).then(function(data) {
     if (data.length) {
       req.flash('message', {
         status: 'danger',
@@ -45,16 +51,39 @@ router.post('/teacher/add', function(req, res, next) {
       res.redirect('/teacher/add');
     } else {
       // hash and salt the password
-      var hashedPassword = helpers.hashing(password);
-      Teachers().insert({
-          email: username,
-          password: hashedPassword
-      }).then(function() {
-          req.flash('message', {status: 'success', value: 'Successfully Registered.'});
-          res.redirect('/');
-      });
+        var hashedPassword = helpers.hashing(password);
+        Teachers().insert({
+            email_address: email,
+            password: hashedPassword
+        }).then(function() {
+            req.flash('message', {status: 'success', value: 'Successfully Registered.'});
+            res.redirect('/');
+        });
       }
     });
+});
+
+
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user) {
+    if (err) {
+      return next(err);
+    } else {
+      req.logIn(user, function(err) {
+        if (err) {
+          return next(err);
+        }
+        req.flash('message', {status: 'success', value: 'Welcome '+ user.first_name});
+        return res.redirect('/');
+      });
+    }
+  })(req, res, next);
+});
+
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  req.flash('message', {status: 'success', value:'Successfully logged out.'});
+  res.redirect('/');
 });
 
 
