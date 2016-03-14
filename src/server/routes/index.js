0080efddc3f50c56c3add2c86b8ada4405e4fb71
Louisa Reese <louisa.e.reese@gmail.com>
2016-03-14 12:47:53 -0600

    added view pages and routes just for visibility

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,4 +5,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/sales', function(req, res, next) {
+  res.render('saleStart', { title: 'Sell Page' });
+});
+
+router.get('/addStudent', function(req, res, next) {
+  res.render('addStudent', { title: 'Add Student(s)' });
+});
+
+router.get('/addEvent', function(req, res, next) {
+  res.render('addEvent', { title: 'Create Event' });
+});
+
+
+
 module.exports = router;
