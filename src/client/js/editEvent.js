64befceb877cce51467d56b839b00fd16c135158
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-15 14:42:52 -0600

    Added modal

--- src/client/js/editEvent.js
+++ src/client/js/editEvent.js
@@ -58,5 +58,16 @@ function stripNulls(string) {
   return string;
 }
 function editGuest() {
-  console.log('hey');
+  console.log('modal!');
+  $('body').append(
+  '<div class="modal"><div class="modal-dialog"><div class="modal-content"><div class="modal-header">',
+  +      '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&close;</button>',
+  +      '<h4 class="modal-title">Edit Guest</h4>',
+  +    '</div>',
+  +   '<div class="modal-body">',
+  +   '</div>',
+  +   '<div class="modal-footer">',
+  +     '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>',
+  +     '<button type="button" class="btn btn-primary">Save changes</button>',
+  +   '</div></div></div></div>');
 }
