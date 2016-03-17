9d0ed5e57e07c6f6c5248e306b56c38c90b3e3b0
BenAychh <ben.tomas.hernandez@gmail.com>
2016-03-17 14:44:00 -0600

    Migration fix

--- src/server/db/migrations/20160315184715_deleteFlags.js
+++ src/server/db/migrations/20160315184715_deleteFlags.js
@@ -2,36 +2,36 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table('events', function(table) {
     table.boolean('deleted').defaultsTo('false');
-    table.date('deleted_date');
+    table.timestamp('deleted_date');
   })
   .then(function() {
     return knex.schema.table('guests', function(table) {
       table.boolean('deleted').defaultsTo('false');
-      table.date('deleted_date');
+      table.timestamp('deleted_date');
     })
   })
   .then(function() {
     return knex.schema.table('schools', function(table) {
       table.boolean('deleted').defaultsTo('false');
-      table.date('deleted_date');
+      table.timestamp('deleted_date');
     })
   })
   .then(function() {
     return knex.schema.table('students', function(table) {
       table.boolean('deleted').defaultsTo('false');
-      table.date('deleted_date');
+      table.timestamp('deleted_date');
     })
   })
   .then(function() {
     return knex.schema.table('teachers', function(table) {
       table.boolean('deleted').defaultsTo('false');
-      table.date('deleted_date');
+      table.timestamp('deleted_date');
     })
   })
   .then(function() {
     return knex.schema.table('tickets', function(table) {
       table.boolean('deleted').defaultsTo('false');
-      table.date('deleted_date');
+      table.timestamp('deleted_date');
     })
   })
 };
